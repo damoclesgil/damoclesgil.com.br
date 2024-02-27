4616e411f35ffda0388fdfba33aad65275a34c58
damoclesgil <damoclesgil@gmail.com>
2024-02-27 01:06:17 -0300

    Fix: errors projects

--- src/utils/getDuration.js
+++ src/utils/getDuration.js
@@ -1,6 +1,6 @@
-import { parseISO, format, intervalToDuration } from "date-fns";
+import { parseISO, intervalToDuration } from "date-fns";
 
-export const getDuration = (startDate: string, endDate: string) => {
+export const getDuration = (startDate, endDate) => {
   const durationObj = intervalToDuration({
     start: parseISO(startDate),
     end: endDate ? parseISO(endDate) : new Date(),
@@ -8,7 +8,7 @@ export const getDuration = (startDate: string, endDate: string) => {
 
   let durationStr = "";
 
-  if (durationObj.years > 1) {
+  if (durationObj && durationObj.years > 1) {
     durationStr = `${durationObj.years} yrs `;
   } else if (durationObj.years === 1) {
     durationStr = `${durationObj.years} yr `;
