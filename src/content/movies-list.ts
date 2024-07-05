6f29c615d04329442a574604cdbd10d8992e9815
damoclesgil <damoclesgil@gmail.com>
2024-07-05 17:46:57 -0300

    Feat: favorites movies

---
+++ src/content/movies-list.ts
@@ -0,0 +1,42 @@
+export const MOVIES_LIST = [
+  {
+    title: "A Quiet Place: Day One",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "Drive",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "A Bug's Life",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "Kingdom of the Planet of the Apes",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "Lucy",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "The Purge",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "Inside Out 2",
+    href: "",
+    imgSrc: "",
+  },
+  {
+    title: "Puss in Boots: The Last Wish",
+    href: "",
+    imgSrc: "",
+  },
+];
